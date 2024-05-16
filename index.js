8c9377ad87fd38c3874ee307f387bc81ea6d6ec7
alijunaid786 <sheikali261998@gmail.com>
2024-05-16 18:10:07 +0500

    Push my calculator to github

---
+++ index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("SIMPLE CALCULATOR");
+let msg = await inquirer.prompt([
+    { message: "Enter First Number", name: "first", type: "number" },
+    { message: "Enter Second Number", name: "second", type: "number" },
+    {
+        message: "Select an operation to perform action",
+        name: "operator",
+        type: "list",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"],
+    },
+]);
+if (msg.operator == "Addition") {
+    console.log(`Your Answer is ${msg.first + msg.second}`);
+}
+if (msg.operator == "Substraction") {
+    console.log(`Your Answer is ${msg.first - msg.second}`);
+}
+if (msg.operator == "Multiplication") {
+    console.log(`Your Answer is ${msg.first * msg.second}`);
+}
+if (msg.operator == "Division") {
+    console.log(`Your Answer is ${msg.first / msg.second}`);
+}
+console.log(msg);
